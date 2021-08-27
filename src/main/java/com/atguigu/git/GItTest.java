14971143f87f5c8b7dd27cdb08c0d81dc8121bf4
xiamo <xiamo@wei.com>
2021-08-27 16:40:00 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GItTest.java
+++ src/main/java/com/atguigu/git/GItTest.java
@@ -7,5 +7,6 @@ public class GItTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("XiaMo");
+        System.out.println("XiaMo2");
     }
 }
